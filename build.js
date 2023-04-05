15cf15f9911ae3718be6658854ba6c3ee07f2190
Admin-growmodo <pm@growmodo.com>
2023-04-05 15:05:24 +0800

    Add comment

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 const StyleDictionaryPackage = require('style-dictionary');
 
-// HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED S
+// HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
 
 StyleDictionaryPackage.registerFormat({
     name: 'scss/variables',
