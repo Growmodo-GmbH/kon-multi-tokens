04a2bca6a0c5cd8a4088afb06290027e6bf83cc5
Admin-growmodo <pm@growmodo.com>
2023-04-10 14:54:06 +0800

    Add new token

--- build.js
+++ build.js
@@ -121,7 +121,7 @@ console.log('Build started...');
 
 // PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
 
-['global', 'dark', 'light'].map(function (theme) {
+['global', 'dark', 'light', 'input'].map(function (theme) {
 
     console.log('\n==============================================');
     console.log(`\nProcessing: [${theme}]`);
